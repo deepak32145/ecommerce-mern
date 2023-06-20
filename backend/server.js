932f32911c476e5da65a1d2879f86d62ec0d6a3e
deepak choudhary <deepak32145@gmail.com>
2023-06-20 18:04:49 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,24 @@ const dotenv = require("dotenv");
 
 const connectDatabase = require("./config/database");
 
+process.on("uncaughtException", (err) => {
+  console.log(`Error : ${err.message}`);
+  console.log("shutting down the server due to uncaught exception");
+  process.exit(1);
+});
+
 dotenv.config({ path: "backend/config/config.env" });
 
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(`Error : ${err.message}`);
+  console.log("shutting down the server due to unhandled promise rejections");
+  server.close(() => {
+    process.exit(1);
+  });
 });
